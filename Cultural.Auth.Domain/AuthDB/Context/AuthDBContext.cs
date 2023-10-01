8fff003ad73b140819a57097c585087970b7e9e2
Oleksiy <oleksiy.kondratyuk.work@gmail.com>
2023-10-01 21:25:05 +0300

    Add project files.

---
+++ Cultural.Auth.Domain/AuthDB/Context/AuthDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Cultural.Auth.Domain.AuthDB.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cultural.Auth.Domain.AuthDB.Context;
+
+public class AuthDBContext : DbContext
+{
+    public DbSet<UserEntity> Users { get; set; }
+
+    public AuthDBContext(DbContextOptions<AuthDBContext> options) : base(options)
+    {
+        try
+        {
+            Database.EnsureCreated();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+        }
+    }
+}
+
+
---
+++ Cultural.Auth.Domain/AuthDB/Entities/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cultural.Auth.Domain.AuthDB.Entities;
+
+public class UserEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+}
