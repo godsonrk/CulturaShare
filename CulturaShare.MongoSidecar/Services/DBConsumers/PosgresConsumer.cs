48ac8d621247d90b98946dafae17d8ab8a2dbf2a
Oleksiy <oleksiy.kondratyuk.work@gmail.com>
2023-11-20 10:23:30 +0300

    update for mongo were added

--- CulturaShare.MongoSidecar/Services/DBConsumers/PosgresConsumer.cs
+++ CulturaShare.MongoSidecar/Services/DBConsumers/PosgresConsumer.cs
@@ -1,84 +1,116 @@
 ﻿using Confluent.Kafka;
 using CulturalShare.Posts.Data.Extensions;
 using CulturalShare.PostWrite.Domain.Context;
-using MongoDB.Bson;
 using MongoDB.Driver;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Linq.Expressions;
 
-namespace CulturaShare.MongoSidecar.Services.DBConsumers;
-
-public class PosgresConsumer : IPostgresConsumer
+namespace CulturaShare.MongoSidecar.Services.DBConsumers
 {
-    public async Task Consume<T> (ConsumerConfig kafkaConfig, Func<PostWriteDBContext> CreateDbContext, IMongoCollection<T> mongoCollection,
-        params Expression<Func<T, object>>[] includes) where T : class
+    public class PosgresConsumer : IPostgresConsumer
     {
-
-        var entityType = typeof(T);
-        var topic = $"source.public.{entityType.GetTableAttributeValue()}";
-        using (var consumer = new ConsumerBuilder<Ignore, string>(kafkaConfig).Build())
+        public async Task Consume<T>(ConsumerConfig kafkaConfig, Func<PostWriteDBContext> createDbContext, IMongoCollection<T> mongoCollection,
+            params Expression<Func<T, object>>[] includes) where T : class
         {
-            consumer.Subscribe(topic);
+            var entityType = typeof(T);
+            var topic = $"source.public.{entityType.GetTableAttributeValue()}";
 
-            CancellationTokenSource cts = new CancellationTokenSource();
-            Console.CancelKeyPress += (_, e) => { e.Cancel = true; cts.Cancel(); };
-
-            try
+            using (var consumer = new ConsumerBuilder<Ignore, string>(kafkaConfig).Build())
             {
-                while (true)
+                consumer.Subscribe(topic);
+
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => { e.Cancel = true; cts.Cancel(); };
+
+                try
                 {
-                    using (var context = CreateDbContext())
+                    while (true)
                     {
-                        var consumeResult = consumer.Consume(cts.Token);
-                        var id = GetIdFromMessage(consumeResult.Message.Value);
-
-                        var entity = await context.GetEntityByIdAsync(id, includes);
-
-                        var serializerSettings = new JsonSerializerSettings
+                        using (var context = createDbContext())
                         {
-                            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                        };
-                        // Convert entity to JSON
-                        string json = JsonConvert.SerializeObject(entity, serializerSettings);
-
-                        // Insert JSON document into MongoDB
-                        var entityFromJson = JsonConvert.DeserializeObject<T>(json);
-                        await mongoCollection.InsertOneAsync(entityFromJson);
+                            await ProcessMessage(mongoCollection, includes, consumer, cts, context);
+                        }
                     }
                 }
+                catch (OperationCanceledException)
+                {
+                    // Ctrl + C was pressed, application is terminating.
+                }
+                catch (Exception ex)
+                {
+                    // Handle specific exceptions or log the error
+                    throw;
+                }
+                finally
+                {
+                    consumer.Close();
+                }
             }
-            catch (OperationCanceledException)
+        }
+
+        private async Task ProcessMessage<T>(
+    IMongoCollection<T> mongoCollection,
+    Expression<Func<T, object>>[] includes,
+    IConsumer<Ignore, string> consumer,
+    CancellationTokenSource cts,
+    PostWriteDBContext context) where T : class
+        {
+            try
             {
-                //Ctrl + C was pressed, application is terminating.
+                var consumeResult = consumer.Consume(cts.Token);
+                var id = GetIdFromMessage(consumeResult.Message.Value);
+
+                // Check if document with the given ID already exists
+                var existingDocument = await mongoCollection.Find(Builders<T>.Filter.Eq("_id", id)).FirstOrDefaultAsync();
+                var entity = await context.GetEntityByIdAsync(id, includes);
+                var mongoEntity = GetMongoEntity(entity);
+                if (existingDocument != null)
+                {
+                    var updateResult = await mongoCollection.ReplaceOneAsync(
+                        Builders<T>.Filter.Eq("_id", id),
+                        mongoEntity,
+                        new ReplaceOptions { IsUpsert = false });
+                }
+                else
+                {
+                    await mongoCollection.InsertOneAsync(mongoEntity);
+                }
             }
             catch (Exception ex)
             {
-                throw;
+                // Handle the exception as needed
+                var errorMessage = ex.Message;
             }
-            finally
+        }
+
+
+        private T GetMongoEntity<T>(T entity)
+        {
+            var serializerSettings = new JsonSerializerSettings
             {
-                consumer.Close();
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            };
+            var json = JsonConvert.SerializeObject(entity, serializerSettings);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        private int GetIdFromMessage(string message)
+        {
+            try
+            {
+                // Parse the JSON string using JObject
+                var jsonObject = JObject.Parse(message);
+
+                // Get the value of the "Id" field
+                var idValue = (int)jsonObject["Id"];
+
+                return idValue;
             }
-        }
-    }
-
-    private int GetIdFromMessage(string message)
-    {
-        try
-        {
-            // Parse the JSON string using JObject
-            JObject jsonObject = JObject.Parse(message);
-
-            // Get the value of the "Id" field
-            var idValue = (int)jsonObject["Id"];
-
-            return idValue;
-        }
-        catch
-        {
-            return 0;
+            catch
+            {
+                return 0;
+            }
         }
     }
 }
-
